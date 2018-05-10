0bececc0fb481f7730d3b837ac22c9683d53e50b
seowzhenjun <30334683+seowzhenjun@users.noreply.github.com>
2018-05-11 01:52:22 +1000

    Edit sidebar UI

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 
@@ -11,41 +11,44 @@ import { CloudFunctionService } from '../../services/cloud-function.service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
   @Input() userDetail : any;
-  
+  sidebarList = [
+    { name : 'Inbox',             icon : 'inbox'},
+    { name : 'Filter',            icon : 'filter_list'},
+    { name : 'Setting',           icon : 'settings'},
+    { name : 'Help and Feedback', icon : 'help'},
+    { name : 'Logout',            icon : 'exit_to_app'}
+  ];
   constructor(
     private router : Router,
     private sidenav : MatSidenav,
     public authService : AuthService,
     private _cf : CloudFunctionService
   ) {}
-  
-  //headerImgUrl : string = this.userDetail.imageUrl;
-  ngOnInit() {
-  }
 
-  inbox(){
-    this.router.navigate(['/main/table']);
-    this.sidenav.close();
-  }
+  navigate(path){
+    switch(path){
+      case 'Inbox':
+        this.router.navigate(['/main/table']);
+        this.sidenav.close();
+      break;
+      case 'Filter':
+        this.router.navigate(['/main/filter']);
+        this.sidenav.close();
+      break;
+      case 'Setting':
+        this.router.navigate(['/main/settings']);
+        this.sidenav.close();
+      break;
+      case 'Help and Feedback':
+        this.router.navigate(['/main/help']);
+        this.sidenav.close();
+      break;
+      case 'Logout':
+        this.authService.logout();
+      break;
+    }
 
-  filter(){
-    this.router.navigate(['/main/filter']);
-    this.sidenav.close();
-  }
-
-  settings(){
-    this.router.navigate(['/main/settings']);
-    this.sidenav.close();
-  }
-
-  help(){
-    this.router.navigate(['/main/help']);
-    this.sidenav.close();
-  }
-
-  signOut(){
-    this.authService.logout();
   }
 }
